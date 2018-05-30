16c57e37596c69166cfb87c85efb4180fff4181c
Griffin Scott <gscott@e1z1r5p33.42.us.org>
2018-05-29 21:31:57 -0700

    yeehaw

---
+++ sudoku/sudoku.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+int isAvailable(int puzzle[][9], int row, int col, int num)
+{
+    int rowStart = (row/3) * 3;
+    int colStart = (col/3) * 3;
+    int i, j;
+	
+	i = 0;
+    while(i < 9)
+    {
+        if (puzzle[row][i] == num) return 0;
+        if (puzzle[i][col] == num) return 0;
+        if (puzzle[rowStart + (i%3)][colStart + (i/3)] == num) return 0;
+		++i;
+    }
+    return 1;
+}
+
+int fillSudoku(int puzzle[][9], int row, int col)
+{
+    int i;
+
+	i = 0;
+    if(row<9 && col<9)
+    {
+        if(puzzle[row][col] != 0)
+        {
+            if((col+1)<9) return fillSudoku(puzzle, row, col+1);
+            else if((row+1)<9) return fillSudoku(puzzle, row+1, 0);
+            else return 1;
+        }
+        else
+        {
+            while(i<9)
+            {
+                if(isAvailable(puzzle, row, col, i+1))
+                {
+                    puzzle[row][col] = i+1;
+                    if((col+1)<9)
+                    {
+                        if(fillSudoku(puzzle, row, col +1)) return 1;
+                        else puzzle[row][col] = 0;
+                    }
+                    else if((row+1)<9)
+                    {
+                        if(fillSudoku(puzzle, row+1, 0)) return 1;
+                        else puzzle[row][col] = 0;
+                    }
+                    else return 1;
+                }
+				++i;
+            }
+        }
+        return 0;
+    }
+    else return 1;
+}
+
+int main()
+{
+    int i, j;
+
+	i = 1;
+	j = 1;
+    int puzzle[9][9]={{0, 0, 0, 0, 0, 0, 0, 9, 0},
+                      {1, 9, 0, 4, 7, 0, 6, 0, 8},
+                      {0, 5, 2, 8, 1, 9, 4, 0, 7},
+                      {2, 0, 0, 0, 4, 8, 0, 0, 0},
+                      {0, 0, 9, 0, 0, 0, 5, 0, 0},
+                      {0, 0, 0, 7, 5, 0, 0, 0, 9},
+                      {9, 0, 7, 3, 6, 4, 1, 8, 0},
+                      {5, 0, 6, 0, 8, 1, 0, 7, 4},
+                      {0, 8, 0, 0, 0, 0, 0, 0, 0}};
+
+    if(fillSudoku(puzzle, 0, 0))
+    {
+        printf("\n+-----+-----+-----+\n");
+        while(i<10)
+        {
+            while(j<10)
+			{
+				printf("|%d", puzzle[i-1][j-1]);
+				++j;
+			}
+			   	printf("|\n");
+            if (i%3 == 0) printf("+-----+-----+-----+\n");
+			++j;
+        }
+    }
+    else printf("\n\nNO SOLUTION\n\n");
+
+    return 0;
+}
