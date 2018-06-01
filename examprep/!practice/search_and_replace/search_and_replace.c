db259a16c3ab1914729455dc3ad71db9308bfbaa
Griffin Scott <gscott@e1z1r5p29.42.us.org>
2018-06-01 04:33:09 -0700

    3 down

--- examprep/!practice/search_and_replace/search_and_replace.c
+++ examprep/!practice/search_and_replace/search_and_replace.c
@@ -1,50 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   repeat_alpha.c                                     :+:      :+:    :+:   */
+/*   search_and_replace.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gscott <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/06/01 03:43:01 by gscott            #+#    #+#             */
-/*   Updated: 2018/06/01 04:19:57 by gscott           ###   ########.fr       */
+/*   Created: 2018/06/01 04:20:17 by gscott            #+#    #+#             */
+/*   Updated: 2018/06/01 04:30:17 by gscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	*repeat_alpha(char letter)
-{
-	int i;
-
-	i = 1;
-	if (letter > 64 && letter < 91)
-		i = (letter - 64);
-	if (letter > 97 && letter < 123)
-		i = (letter - 96);
-	while (i > 0)
-	{
-		write(1, &letter, 1);
-		i--;
-	}
-	return (0);
-}
-
 int	main(int argc, char **argv)
 {
-
 	int i;
 	char c;
-	
+
 	i = 0;
-	c = '\n';
-	if (argc == 2)
+	if (argc == 4)
 	{
 		while (argv[1][i])
 		{
-			repeat_alpha(argv[1][i]);
+			if (argv[1][i] == argv[2][0])
+				argv[1][i] = argv[3][0];
+			c = argv[1][i];
+			write(1, &c, 1);
 			i++;
 		}
 	}
-	write(1, &c, 1);
+	write(1, "\n", 1);
 	return (0);
 }
